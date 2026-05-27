faff3854222eea741940db34a35dd32e94e6d15c
Lucas Vizoto <lucasvizoto364@gmail.com>
2026-05-27 18:52:29 -0300

    [FEAT]: Implementando alterações visuais no Swagger

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -12,8 +12,23 @@ export class AuthController {
 
     @Post('login')
     @HttpCode(HttpStatus.OK)
-    @ApiOperation({ summary: 'User login' }) //Documentação dinâmica do swagger
-    @ApiResponse({ status: 200, description: 'Login successful' })
+    @ApiOperation({
+        summary: 'User login',
+        description: 'Autentica um usuário e retorna um token JWT para acesso aos recursos protegidos.'
+    }) //Documentação dinâmica do swagger
+    @ApiResponse({
+        status: 200,
+        description: 'Login successful',
+        schema: {
+            type: 'object',
+            properties: {
+                user: { type: 'object', },
+                accessToken: { type: 'string' },
+                sessionToken: { type: 'string' },
+                expiresIn: { type: 'number' },
+            },
+        },
+    })
     @ApiResponse({ status: 401, description: 'Invalid credentials' })
     @Throttle({ short: { ttl: 60000, limit: 5 } }) // Limita a 5 tentativas de login por minuto
     async login(@Body() loginDto: LoginDto) {
@@ -22,9 +37,13 @@ export class AuthController {
 
     @Post('register')
     @HttpCode(HttpStatus.CREATED)
-    @ApiOperation({ summary: 'User registration' })
+    @ApiOperation({
+        summary: 'User registration',
+        description: 'Cria uma nova conta de usuário no sistema.'
+    })
     @ApiResponse({ status: 201, description: 'Registration successful' })
     @ApiResponse({ status: 400, description: 'Invalid registration data' })
+    @ApiResponse({ status: 409, description: 'Email já cadastrado' })
     @Throttle({ medium: { ttl: 60000, limit: 3 } }) // Limita a 3 tentativas de login por minuto
     async register(@Body() registerDto: RegisterDto) {
         return this.authService.register(registerDto);
